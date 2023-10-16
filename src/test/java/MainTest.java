a9d2fc2266b1ce8c9b3867edf6027e1a6a0f45b7
Marcin Szymajda <marcin.szymajda@boat-systems.pl>
2023-10-16 17:50:43 +0200

    test for repositories

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,10 @@
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.example.entity.Client;
+import org.example.entity.*;
+import org.example.repository.ClientRepository;
+import org.example.repository.CourtRepository;
+import org.example.repository.RentRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
@@ -12,6 +15,18 @@ public class MainTest {
 
     private static EntityManagerFactory emf;
     private static EntityManager em;
+
+    private ClientRepository ClientRepo = new ClientRepository();
+    private CourtRepository CourtRepo = new CourtRepository();
+    private RentRepository RentRepo = new RentRepository();
+
+    Client c1 = new Client("Marcin","Szymajda","123");
+    Client c2 = new Client("Jakub","Osypiuk","456");
+    Client c3 = new Client("Dominik","Marzec","123567");
+
+    Court co1 = new FootballCourt(25.5,12.3,3.25,2.25);
+    Court co2 = new BasketballCourt(22.4,21.5,2.8,1.1);
+    Court co3 = new VolleyballCourt(15.2,13.2,1.25,15.0);
 
     @BeforeAll
     static void beforeAll() {
@@ -27,24 +42,45 @@ public class MainTest {
     }
 
     @Test
-    public void testConn() {
+    public void rentRepoTest() {
 
-        //public Client(String firstName, String lastName, String personalID, int clientID) {
-//    Client client = new Client("Marcin","Szymajda","123",1);
-//    em.getTransaction().begin();
-//    em.persist(client);
-//    em.getTransaction().commit();
+    }
+    @Test
+    public void courtRepoTest() {
+
+
+        Assert.assertTrue(CourtRepo.add(co1));
+        Assert.assertTrue(CourtRepo.add(co2));
+        Assert.assertTrue(CourtRepo.add(co3));
+
+        Assert.assertEquals(CourtRepo.find(2),co2);
+
+        Assert.assertEquals(CourtRepo.findAll().size(),3);
+
+        Assert.assertTrue(CourtRepo.remove(2));
+
+        Assert.assertEquals(CourtRepo.findAll().size(),2);
+
     }
 
     @Test
-    public void basicAssert() {
+    public void clientRepoTest() {
 
-        Assert.assertEquals("Hello","Hello");
+
+    Assert.assertTrue(ClientRepo.add(c1));
+    Assert.assertTrue(ClientRepo.add(c2));
+    Assert.assertTrue(ClientRepo.add(c3));
+
+    Assert.assertEquals(ClientRepo.find(2),c2);
+
+    Assert.assertEquals(ClientRepo.findAll().size(),3);
+
+    Assert.assertTrue(ClientRepo.remove(2));
+
+    Assert.assertEquals(ClientRepo.findAll().size(),2);
+
+
     }
 
-    @Test
-    public void testDB() {
 
-
-    }
 }
