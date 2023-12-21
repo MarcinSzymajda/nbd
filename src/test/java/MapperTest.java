0d083446f23359a57365ce222347d5f5ed5361e4
j-osypiuk <kuba6535@gmail.com>
2023-12-21 10:58:52 +0100

    Cassandra final v1.1

--- src/test/java/MapperTest.java
+++ src/test/java/MapperTest.java
@@ -1,60 +1,72 @@
+import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.cql.Row;
-import org.example.entity.*;
-import org.example.mapper.CourtCustomMapper;
-import org.example.repository.ClientRepository;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.relation.Relation;
+import com.datastax.oss.driver.api.querybuilder.select.Select;
+import org.example.entity.BasketballCourt;
+import org.example.entity.FootballCourt;
+import org.example.entity.VolleyballCourt;
+import org.example.names.CourtIds;
 import org.example.repository.CourtRepository;
-import org.example.repository.RentManager;
-import org.junit.Test;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
+import static org.example.mapper.CourtCustomMapper.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.*;
 
 public class MapperTest {
 
-    Client c1 = new Client(1,false,"Marcin","Szymajda","123");
-    Client c2 = new Client(2,false,"Jakub","Osypiuk","456");
-    Client c3 = new Client(3,false,"Dominik","Marzec","123567");
+    FootballCourt footballCourt = new FootballCourt(100,1.1,1.1,1,1,true, "");
+    VolleyballCourt volleyballCourt = new VolleyballCourt(200,2.2,2.2,1,1,true, "");
+    BasketballCourt basketballCourt = new BasketballCourt(300,3.3,3.3,1,1,true, "");
 
-    FootballCourt footballCourt = new FootballCourt(100,1.1,1.1,1,1,false, "");
-    VolleyballCourt volleyballCourt = new VolleyballCourt(200,2.2,2.2,1,1,false, "");
-    BasketballCourt basketballCourt = new BasketballCourt(300,3.3,3.3,1,1,false, "");
+    @AfterEach
+    void tearDown() {
+        try(CourtRepository courtRepository = new CourtRepository()) {
 
-    Rent r1 = new Rent(999,100,3, LocalDate.of(2022,10,10), null);
-    Rent r2 = new Rent(899,200,2, LocalDate.of(2021,11,11), null);
-    Rent r3 = new Rent(799,300,1, LocalDate.of(2020,12,12), null);
-
-
-    @Test
-    @Order(1)
-    public void createTest() {
-        try(ClientRepository clientRepository = new ClientRepository();
-            RentManager rentRepository = new RentManager();
-            CourtRepository courtRepository = new CourtRepository()) {
-
-            assertTrue(clientRepository.add(c1));
-            assertTrue(clientRepository.add(c2));
-            assertTrue(clientRepository.add(c3));
-
-            assertTrue(courtRepository.add(footballCourt));
-            assertTrue(courtRepository.add(basketballCourt));
-            assertTrue(courtRepository.add(volleyballCourt));
-
-            assertTrue(rentRepository.add(r1));
-            assertTrue(rentRepository.add(r2));
-            assertTrue(rentRepository.add(r3));
-
-        } catch(Exception e) {
-            return;
+            courtRepository.remove(footballCourt.getId());
+            courtRepository.remove(volleyballCourt.getId());
+            courtRepository.remove(basketballCourt.getId());
         }
     }
 
-
     @Test
-    @Order(2)
-    public void courtMapperTest() {
+    public void customMapperTest() {
+        try(CourtRepository courtRepository = new CourtRepository()) {
+            courtRepository.add(footballCourt);
+            courtRepository.add(volleyballCourt);
+            courtRepository.add(basketballCourt);
+
+            Select selectFootballCourt = QueryBuilder.selectFrom(CqlIdentifier.fromCql("courts"))
+                    .all()
+                    .where(Relation.column(CourtIds.ID).isEqualTo(literal(footballCourt.getId())));
+
+            Row footballRow = courtRepository.getSession().execute(selectFootballCourt.build()).one();
+            FootballCourt mappedFootball = getFootballCourt(footballRow);
+
+            assertEquals(footballCourt, mappedFootball);
 
 
+            Select selectBasketballCourt = QueryBuilder.selectFrom(CqlIdentifier.fromCql("courts"))
+                    .all()
+                    .where(Relation.column(CourtIds.ID).isEqualTo(literal(basketballCourt.getId())));
+
+            Row basketballRow = courtRepository.getSession().execute(selectBasketballCourt.build()).one();
+            BasketballCourt mappedBasketball = getBasketballCourt(basketballRow);
+
+            assertEquals(basketballCourt, mappedBasketball);
+
+
+            Select selectVolleyballCourt = QueryBuilder.selectFrom(CqlIdentifier.fromCql("courts"))
+                    .all()
+                    .where(Relation.column(CourtIds.ID).isEqualTo(literal(volleyballCourt.getId())));
+
+            Row volleyballRow = courtRepository.getSession().execute(selectVolleyballCourt.build()).one();
+            VolleyballCourt mappedVolleyball = getVolleyballCourt(volleyballRow);
+
+            assertEquals(volleyballCourt, mappedVolleyball);
+        }
     }
 }
